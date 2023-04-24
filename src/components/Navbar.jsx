2ea869374a9817d230e5bdae92ed41c137ac7efd
dagmon2 <daniel.agmon@gmail.com>
2023-04-24 22:10:35 +0300

    Mid-way of creating the Navbar. Added Tailwind css required depdencies

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+import { styles } from "../styles";
+import { navLinks } from "../constants";
+import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div>
+      <nav
+        className={`${styles.paddingX}
+      w-full
+      flex
+      items-center
+      py-5
+      fixed
+      top-0
+      z-20
+      bg-primary
+      `}
+      >
+        <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+          <Link
+            to="/"
+            className="flex items-center gap-2"
+            onClick={() => {
+              setActive("");
+              window.scrollTo(0, 0);
+            }}
+          >
+            <img className="w-9 h-9 object-contain" src={logo} alt="logo" />
+            <p className="text-white text-[18px] font-bold cursor-pointer">
+              Daniel{" "}
+              <span className="sm:block hidden">| Full Stack Engineer </span>
+            </p>
+          </Link>
 
-export default Navbar
+          <ul></ul>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
